95f87f5dbd4856e7b93bb34bf4e90bb8e5c864dc
asimzhanovvv <radmir140f@gmail.com>
2025-03-02 23:59:05 +0500

    Lab6 3rd commit

--- Labs/Lab6/dir-and-files/Task8.py
+++ Labs/Lab6/dir-and-files/Task8.py
@@ -3,19 +3,11 @@ import os
 
 path = r"C:\Users\Asus Tuf\Desktop\Files\PP2_Github\Labs\Lab6\dir-and-files\Task8.txt"
 
-
-if os.access(path, os.F_OK): # Check if file exists
-    if os.access(path, os.W_OK): # Check access to delete
-        os.remove(path)
-        print("[File was deleted]")
-    else:
-        print("[You don't have access to delete this file]")
-
-# For Fun :)
-else:
-    print("[File was not found]")
+# Just a joke
+def joke():
     time.sleep(1)
-    print("[But I will create it for you]")
+    print("[But I will create New file for you]")
+    time.sleep(1)
     open(path, 'w')
 
     for i in range(5, 0, -1):
@@ -26,4 +18,16 @@ else:
     time.sleep(1)
 
     os.remove(path)
-    print("[File was created and deleted]")
+    print("[File was created and deleted]")   
+
+if os.access(path, os.F_OK): # Check if file exists
+    if os.access(path, os.W_OK): # Check access to delete
+        os.remove(path)
+        print("[File was deleted]")
+    else:
+        print("[You don't have access to delete this file]")
+        joke()
+
+else:
+    print("[File was not found]")
+    joke()
